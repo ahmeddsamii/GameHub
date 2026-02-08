456a9aeb20b25d7a097a3dabc162a4ffd416f369
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-08 22:37:18 +0200

    Add GameRepository interface with getGames function and Koin annotation

---
+++ domain/src/main/java/com/example/domain/repository/GameRepository.kt
@@ -0,0 +1,9 @@
+package com.example.domain.repository
+
+import com.example.domain.entity.Game
+import org.koin.core.annotation.Single
+
+@Single(binds = [GameRepository::class])
+interface GameRepository {
+    suspend fun getGames(): List<Game>
+}
