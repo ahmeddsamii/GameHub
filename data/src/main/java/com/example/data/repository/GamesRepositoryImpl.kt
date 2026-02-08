82a3e47b7e75b602b6ddec103147d2215e545e27
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-08 22:37:30 +0200

    Implement GamesRepositoryImpl for fetching games with Ktor and Koin DI

---
+++ data/src/main/java/com/example/data/repository/GamesRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.example.data.repository
+
+import com.example.data.remote.dto.GameDto
+import com.example.data.remote.dto.PaginationResponse
+import com.example.data.remote.mapper.toDomain
+import com.example.data.utils.safeApiCall
+import com.example.domain.entity.Game
+import com.example.domain.repository.GameRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import org.koin.core.annotation.Single
+
+@Single(binds = [GameRepository::class])
+class GamesRepositoryImpl(
+    private val httpClient: HttpClient
+): GameRepository {
+    override suspend fun getGames(): List<Game> {
+        val response = safeApiCall<PaginationResponse<GameDto>> {
+            httpClient.get("games") {
+                parameter("page", 1)
+            }.body()
+        }
+        return response.results.map { it.toDomain() }
+    }
+}
