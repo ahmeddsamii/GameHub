23607303e78e51c9af97e4c29c689a8e185ede96
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-08 23:17:19 +0200

    Update HomeUiState to support paginated data for games using PagingData and Flow

--- presentation/src/main/java/com/example/presentation/feature/home/HomeUiState.kt
+++ presentation/src/main/java/com/example/presentation/feature/home/HomeUiState.kt
@@ -1,10 +1,13 @@
 package com.example.presentation.feature.home
 
+import androidx.paging.PagingData
 import com.example.domain.entity.Game
 import com.example.presentation.shared.base.ErrorState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class HomeUiState(
     val isLoading: Boolean = false,
-    val games: List<Game> = emptyList(),
+    val games: Flow<PagingData<Game>> = emptyFlow(),
     val error: ErrorState? = null
 )
