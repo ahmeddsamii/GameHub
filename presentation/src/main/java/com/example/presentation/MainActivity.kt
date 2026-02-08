ca38a47b4273a5029b25fc099fccfa27d0720d97
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-08 22:40:27 +0200

    Move MainActivity from app module to presentation module and integrate NavHost

---
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.presentation.navigation.NavHost
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavHost()
+        }
+    }
+}
