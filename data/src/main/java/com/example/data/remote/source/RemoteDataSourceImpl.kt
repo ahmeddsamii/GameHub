0d49e2850174a5469d254aaac14488c86288428e
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 00:52:20 +0200

    Implement RemoteDataSource using Ktor for fetching games with pagination support

---
+++ data/src/main/java/com/example/data/remote/source/RemoteDataSourceImpl.kt
@@ -0,0 +1,27 @@
+package com.example.data.remote.source
+
+import com.example.data.remote.dto.GameDto
+import com.example.data.remote.dto.PaginationResponse
+import com.example.data.remote.mapper.toDomain
+import com.example.data.utils.safeApiCall
+import com.example.domain.entity.Game
+import com.example.domain.source.RemoteDataSource
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import org.koin.core.annotation.Single
+
+@Single(binds = [RemoteDataSource::class])
+class RemoteDataSourceImpl(
+    private val httpClient: HttpClient
+): RemoteDataSource {
+    override suspend fun getGames(pageNumber: Int): List<Game> {
+        val response = safeApiCall<PaginationResponse<GameDto>> {
+            httpClient.get("games") {
+                parameter("page", pageNumber)
+            }.body()
+        }
+        return response.results.map { it.toDomain() }
+    }
+}
