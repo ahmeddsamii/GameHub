3a546831cb41f2cc3ea3385b159a4d5dcd28b9d7
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-08 22:35:18 +0200

    Add custom domain exceptions for network and authorization errors

---
+++ domain/src/main/java/com/example/domain/exception/Exception.kt
@@ -0,0 +1,5 @@
+package com.example.domain.exception
+
+class NoInternetException(message: String = ""): Exception(message)
+class UnauthorizedException(message: String = ""): Exception(message)
+class NotFoundException(message: String = ""): Exception(message)
