5d4ef61ec70f651aa8fd22295a2689b3af516492
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 09:24:36 +0200

    Update GameRepository to support genre filtering and fetching genre list

--- domain/src/main/java/com/example/domain/repository/GameRepository.kt
+++ domain/src/main/java/com/example/domain/repository/GameRepository.kt
@@ -1,9 +1,11 @@
 package com.example.domain.repository
 
 import com.example.domain.entity.Game
+import com.example.domain.entity.Genre
 import org.koin.core.annotation.Single
 
 @Single(binds = [GameRepository::class])
 interface GameRepository {
-    suspend fun getGames(pageNumber: Int): List<Game>
+    suspend fun getGames(pageNumber: Int, genres: String? = null): List<Game>
+    suspend fun getGenres(pageNumber: Int): List<Genre>
 }
