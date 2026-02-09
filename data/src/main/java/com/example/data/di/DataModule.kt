ff8bc37c0d294625da3bdf9329da965554a7e35f
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 22:28:48 +0200

    Update DataModule to provide Room database and DAO instances

--- data/src/main/java/com/example/data/di/DataModule.kt
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -1,8 +1,32 @@
 package com.example.data.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.data.local.database.GamesDatabase
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("com.example.data")
-class DataModule
+class DataModule{
+    @Single
+    fun provideGamesDatabase(context: Context): GamesDatabase {
+        return Room.databaseBuilder(
+            context,
+            GamesDatabase::class.java,
+            "games_database"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Single
+    fun provideGameDao(database: GamesDatabase) = database.gameDao()
+
+    @Single
+    fun provideGenreDao(database: GamesDatabase) = database.genreDao()
+
+    @Single
+    fun provideGameDetailsDao(database: GamesDatabase) = database.gameDetailsDao()
+}
