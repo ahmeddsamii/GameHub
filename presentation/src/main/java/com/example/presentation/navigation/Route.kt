69f3746c3d058fa7845dc41399c98ccfd7aeb280
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 12:15:03 +0200

    Add GameDetails route with gameId parameter to navigation routes

--- presentation/src/main/java/com/example/presentation/navigation/Route.kt
+++ presentation/src/main/java/com/example/presentation/navigation/Route.kt
@@ -6,4 +6,7 @@ import kotlinx.serialization.Serializable
 sealed interface Route {
     @Serializable
     data object Home : Route
+
+    @Serializable
+    data class GameDetails(val gameId: Int) : Route
 }
