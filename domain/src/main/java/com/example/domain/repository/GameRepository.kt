cc27992de45ce522dc053e727b70904fe7a5b247
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 14:38:43 +0200

    Add getGameDetailsById to GameRepository interface

--- domain/src/main/java/com/example/domain/repository/GameRepository.kt
+++ domain/src/main/java/com/example/domain/repository/GameRepository.kt
@@ -1,6 +1,7 @@
 package com.example.domain.repository
 
 import com.example.domain.entity.Game
+import com.example.domain.entity.GameDetails
 import com.example.domain.entity.Genre
 import org.koin.core.annotation.Single
 
@@ -8,4 +9,5 @@ import org.koin.core.annotation.Single
 interface GameRepository {
     suspend fun getGames(pageNumber: Int, genres: String? = null): List<Game>
     suspend fun getGenres(pageNumber: Int): List<Genre>
+    suspend fun getGameDetailsById(id: Int): GameDetails
 }
