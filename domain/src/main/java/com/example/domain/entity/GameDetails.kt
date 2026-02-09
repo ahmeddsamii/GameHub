5f628e4c3c61f9af42ac24ad585d18821b4cbf0c
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 14:35:26 +0200

    feat: Add GameDetails entity to domain module

---
+++ domain/src/main/java/com/example/domain/entity/GameDetails.kt
@@ -0,0 +1,9 @@
+package com.example.domain.entity
+
+data class GameDetails(
+    val name: String,
+    val backgroundImageUrl: String,
+    val releaseDate: String,
+    val rating: Double,
+    val description: String,
+)
