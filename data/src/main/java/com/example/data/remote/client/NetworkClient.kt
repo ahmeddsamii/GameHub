b9fcc5c03a7cf29413bfa2ab2544ba1cd18883b4
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 09:30:55 +0200

    Configure Ktor NetworkClient with custom logging and default request parameters for RAWG API

--- data/src/main/java/com/example/data/remote/client/NetworkClient.kt
+++ data/src/main/java/com/example/data/remote/client/NetworkClient.kt
@@ -1,10 +1,10 @@
 package com.example.data.remote.client
 
+import com.example.data.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -23,9 +23,18 @@ fun provideHttpClient(): HttpClient = HttpClient(CIO) {
     }
 
     install(Logging) {
-        logger = Logger.DEFAULT
+        logger = object : Logger {
+            override fun log(message: String) {
+                android.util.Log.d("KTOR_LOG", message)
+            }
+        }
         level = LogLevel.ALL
     }
 
-
+    defaultRequest {
+        url("https://api.rawg.io/api/")
+        url {
+            parameters.append("key", BuildConfig.API_KEY)
+        }
+    }
 }
