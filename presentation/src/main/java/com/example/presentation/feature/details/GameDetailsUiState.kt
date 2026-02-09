439c6c89df670f527b63fe7bbb63f98101245a30
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 14:38:05 +0200

    Update GameDetailsUiState to include loading, error, and game details data states

--- presentation/src/main/java/com/example/presentation/feature/details/GameDetailsUiState.kt
+++ presentation/src/main/java/com/example/presentation/feature/details/GameDetailsUiState.kt
@@ -1,4 +1,10 @@
 package com.example.presentation.feature.details
 
-class GameDetailsUiState {
-}
+import com.example.domain.entity.GameDetails
+import com.example.presentation.shared.base.ErrorState
+
+data class GameDetailsUiState(
+    val isLoading: Boolean = true,
+    val error: ErrorState? = null,
+    val gameDetails: GameDetails? = null,
+)
