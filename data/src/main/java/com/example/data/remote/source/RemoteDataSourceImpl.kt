a24945272c47e298bd5a1b783c5325a874dc7abe
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 14:39:53 +0200

    feat: Add game details fetching logic

--- data/src/main/java/com/example/data/remote/source/RemoteDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/source/RemoteDataSourceImpl.kt
@@ -1,11 +1,13 @@
 package com.example.data.remote.source
 
+import com.example.data.remote.dto.GameDetailsDto
 import com.example.data.remote.dto.GameDto
 import com.example.data.remote.dto.GenreDto
 import com.example.data.remote.dto.PaginationResponse
 import com.example.data.remote.mapper.toDomain
 import com.example.data.utils.safeApiCall
 import com.example.domain.entity.Game
+import com.example.domain.entity.GameDetails
 import com.example.domain.entity.Genre
 import com.example.domain.source.RemoteDataSource
 import io.ktor.client.HttpClient
@@ -40,5 +42,12 @@ class RemoteDataSourceImpl(
             }.body()
         }
         return response.results.map { it.toDomain() }
+    }
+
+    override suspend fun getGameDetailsById(id: Int): GameDetails {
+        val response = safeApiCall<GameDetailsDto> {
+            httpClient.get("games/$id").body()
+        }
+        return response.toDomain()
     }
 }
--- domain/src/main/java/com/example/domain/source/RemoteDataSource.kt
+++ domain/src/main/java/com/example/domain/source/RemoteDataSource.kt
@@ -1,9 +1,11 @@
 package com.example.domain.source
 
 import com.example.domain.entity.Game
+import com.example.domain.entity.GameDetails
 import com.example.domain.entity.Genre
 
 interface RemoteDataSource {
     suspend fun getGames(pageNumber: Int, genres: String? = null): List<Game>
     suspend fun getGenres(pageNumber: Int): List<Genre>
+    suspend fun getGameDetailsById(id: Int): GameDetails
 }
