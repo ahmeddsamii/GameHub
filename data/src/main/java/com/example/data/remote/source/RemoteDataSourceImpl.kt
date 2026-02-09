607d3c7f670d543803567d1943b9eb3d18936b5c
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 09:31:23 +0200

    Update RemoteDataSource and its implementation to support genre filtering and fetching genres

--- data/src/main/java/com/example/data/remote/source/RemoteDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/source/RemoteDataSourceImpl.kt
@@ -1,10 +1,12 @@
 package com.example.data.remote.source
 
 import com.example.data.remote.dto.GameDto
+import com.example.data.remote.dto.GenreDto
 import com.example.data.remote.dto.PaginationResponse
 import com.example.data.remote.mapper.toDomain
 import com.example.data.utils.safeApiCall
 import com.example.domain.entity.Game
+import com.example.domain.entity.Genre
 import com.example.domain.source.RemoteDataSource
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -15,10 +17,25 @@ import org.koin.core.annotation.Single
 @Single(binds = [RemoteDataSource::class])
 class RemoteDataSourceImpl(
     private val httpClient: HttpClient
-): RemoteDataSource {
-    override suspend fun getGames(pageNumber: Int): List<Game> {
+) : RemoteDataSource {
+    override suspend fun getGames(
+        pageNumber: Int,
+        genres: String?
+    ): List<Game> {
         val response = safeApiCall<PaginationResponse<GameDto>> {
             httpClient.get("games") {
+                parameter("page", pageNumber)
+                parameter("genres", genres)
+            }.body()
+        }
+        return response.results.map { it.toDomain() }
+    }
+
+    override suspend fun getGenres(
+        pageNumber: Int,
+    ): List<Genre> {
+        val response = safeApiCall<PaginationResponse<GenreDto>> {
+            httpClient.get("genres") {
                 parameter("page", pageNumber)
             }.body()
         }
--- domain/src/main/java/com/example/domain/source/RemoteDataSource.kt
+++ domain/src/main/java/com/example/domain/source/RemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.domain.source
 
 import com.example.domain.entity.Game
+import com.example.domain.entity.Genre
 
 interface RemoteDataSource {
-    suspend fun getGames(pageNumber: Int): List<Game>
+    suspend fun getGames(pageNumber: Int, genres: String? = null): List<Game>
+    suspend fun getGenres(pageNumber: Int): List<Genre>
 }
