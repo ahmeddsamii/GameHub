bcbfb926b64d9f0825616ac73a0f051b630202ec
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 14:59:42 +0200

    Remove Koin @Single annotation from GameRepository interface

--- domain/src/main/java/com/example/domain/repository/GameRepository.kt
+++ domain/src/main/java/com/example/domain/repository/GameRepository.kt
@@ -3,9 +3,7 @@ package com.example.domain.repository
 import com.example.domain.entity.Game
 import com.example.domain.entity.GameDetails
 import com.example.domain.entity.Genre
-import org.koin.core.annotation.Single
 
-@Single(binds = [GameRepository::class])
 interface GameRepository {
     suspend fun getGames(pageNumber: Int, genres: String? = null): List<Game>
     suspend fun getGenres(pageNumber: Int): List<Genre>
