15fcce8d47f6ee3601821bcdb84865a2d7f482d8
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 09:28:14 +0200

    Add genres paging data to HomeUiState

--- presentation/src/main/java/com/example/presentation/feature/home/HomeUiState.kt
+++ presentation/src/main/java/com/example/presentation/feature/home/HomeUiState.kt
@@ -2,6 +2,7 @@ package com.example.presentation.feature.home
 
 import androidx.paging.PagingData
 import com.example.domain.entity.Game
+import com.example.domain.entity.Genre
 import com.example.presentation.shared.base.ErrorState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
@@ -9,5 +10,6 @@ import kotlinx.coroutines.flow.emptyFlow
 data class HomeUiState(
     val isLoading: Boolean = false,
     val games: Flow<PagingData<Game>> = emptyFlow(),
+    val genres: Flow<PagingData<Genre>> = emptyFlow(),
     val error: ErrorState? = null
 )
