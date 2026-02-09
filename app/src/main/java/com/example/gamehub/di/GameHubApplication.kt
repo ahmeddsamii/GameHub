bc1b08439aa900c54a8bf3f89d2509f4af217630
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 14:59:11 +0200

    Add DomainModule to Koin configuration in GameHubApplication

--- app/src/main/java/com/example/gamehub/di/GameHubApplication.kt
+++ app/src/main/java/com/example/gamehub/di/GameHubApplication.kt
@@ -2,6 +2,7 @@ package com.example.gamehub.di
 
 import android.app.Application
 import com.example.data.di.DataModule
+import com.example.domain.di.DomainModule
 import com.example.presentation.di.PresentationModule
 import org.koin.core.context.startKoin
 import org.koin.ksp.generated.module
@@ -11,7 +12,7 @@ class GameHubApplication: Application() {
         super.onCreate()
 
         startKoin {
-            modules(DataModule().module, PresentationModule().module)
+            modules(DataModule().module, DomainModule().module, PresentationModule().module)
         }
     }
 }
