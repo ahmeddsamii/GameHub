9a6b8955d676cf112b5f6aa55aa89ef4c45454c0
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 22:33:26 +0200

    Update Koin configuration to provide Application Context in GameHubApplication

--- app/src/main/java/com/example/gamehub/di/GameHubApplication.kt
+++ app/src/main/java/com/example/gamehub/di/GameHubApplication.kt
@@ -5,6 +5,7 @@ import com.example.data.di.DataModule
 import com.example.domain.di.DomainModule
 import com.example.presentation.di.PresentationModule
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 import org.koin.ksp.generated.module
 
 class GameHubApplication: Application() {
@@ -12,7 +13,14 @@ class GameHubApplication: Application() {
         super.onCreate()
 
         startKoin {
-            modules(DataModule().module, DomainModule().module, PresentationModule().module)
+            modules(
+                module {
+                    single<android.content.Context> { this@GameHubApplication }
+                },
+                DataModule().module,
+                DomainModule().module,
+                PresentationModule().module
+            )
         }
     }
 }
