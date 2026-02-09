add6cac47c2b676a4b9854a8bfa72415bcd9a952
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 11:55:36 +0200

    Add EmptyState component for displaying no result states

---
+++ presentation/src/main/java/com/example/presentation/shared/component/EmptyState.kt
@@ -0,0 +1,31 @@
+package com.example.presentation.shared.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.presentation.R
+
+@Composable
+fun EmptyState(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        AsyncImage(
+            model = R.drawable.ic_no_result,
+            contentDescription = "No Result Icon",
+            modifier = Modifier.size(150.dp)
+        )
+
+        Text("No Result Found!", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+    }
+}
