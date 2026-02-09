7eab0b7be8390b8eeb35542337fec9d3fc1b4c76
ahmeddsamii <ahmbdelkhalek@gmail.com>
2026-02-09 09:25:04 +0200

    Update GamesRepositoryImpl to support genre filtering and implement getGenres method

--- data/src/main/java/com/example/data/repository/GamesRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/GamesRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.data.repository
 
 import com.example.domain.entity.Game
+import com.example.domain.entity.Genre
 import com.example.domain.repository.GameRepository
 import com.example.domain.source.RemoteDataSource
 import org.koin.core.annotation.Single
@@ -8,8 +9,12 @@ import org.koin.core.annotation.Single
 @Single(binds = [GameRepository::class])
 class GamesRepositoryImpl(
     private val remoteDataSource: RemoteDataSource
-): GameRepository {
-    override suspend fun getGames(pageNumber: Int): List<Game> {
-        return remoteDataSource.getGames(pageNumber)
+) : GameRepository {
+    override suspend fun getGames(pageNumber: Int, genres: String?): List<Game> {
+        return remoteDataSource.getGames(pageNumber, genres)
+    }
+
+    override suspend fun getGenres(pageNumber: Int): List<Genre> {
+        return remoteDataSource.getGenres(pageNumber)
     }
 }
